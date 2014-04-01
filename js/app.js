b3a8ef5c8c3b249bea80c568cbbeca9e46a8041d
Jia-He Jou <joujiahe@gmail.com>
2014-04-01 22:44:20 +0800

    init
    Signed-off-by: Jia-He Jou <joujiahe@gmail.com>

---
+++ js/app.js
@@ -0,0 +1,62 @@
+var chattyServer = 'http://api.chatty';
+
+angular.module('app', [])
+
+.factory('sockjs', [function() {
+    return new SockJS(chattyServer);
+}])
+
+.run([function() {
+
+}])
+
+.controller('main', ['$scope', 'sockjs', function($scope, sockjs) {
+    $scope.count = 0;
+    $scope.name  = 'Guest';
+    $scope.messages = [{
+        name: 'chatty',
+        msg:'Welome to Chatty!',
+    }];
+
+    $scope.send = function () {
+        var message = {
+            name: $scope.name,
+            msg: $scope.message,
+            type: 'msg'
+        };
+        sockjs.send(JSON.stringify(message));
+
+        $scope.message = '';
+    };
+    sockjs.onopen = function () {
+        console.log('Connected!');
+    };
+    sockjs.onmessage = function (e) {
+        console.log(e);
+        var data = JSON.parse(e.data);
+        $scope.$apply(function() {
+            switch (data.type) {
+            case 'count':
+                onCount(data.count);
+                break;
+            case 'msg':
+                onMessage(data);
+                break;
+            default:
+                console.log(data);
+                break;
+            }
+        });
+    };
+
+    var onCount = function (count) {
+        $scope.name += count;
+        $scope.count = count;
+        onCount = function (count) {
+            $scope.count = count;
+        }
+    };
+    var onMessage = function (message) {
+        $scope.messages.push(message);
+    };
+}]);
